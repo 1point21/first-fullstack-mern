6a59560feff5eeca0d461b150275d310b0e05596
1point21 <j.drinkall@mailfence.com>
2024-02-14 08:34:14 +0000

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+// use built in middleware to serve up a static file
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    }
+    else if (req.accepts('json')) {
+        res.json({message: '404 not found'})
+    }
+    else {
+        res.type('txt').send('404 not found')
+    }
+})
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
