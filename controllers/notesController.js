edd7d3a5f9da69119f0b57676d82849f50f1ade0
1point21 <j.drinkall@mailfence.com>
2024-02-16 08:59:11 +0000

    feat: add method for getAllNotes

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -5,12 +5,22 @@ const asyncHandler = require("express-async-handler");
 // @desc Get all notes
 // @route GET /notes
 // @access Private
-const getAllNotes = asyncHandler(async (req, res) => {});
+const getAllNotes = asyncHandler(async (req, res) => {
+    const notes = await Note.find().lean()
+    if (!notes?.length) {
+        return res.status(400).json({ message: "No notes found" })
+    }
+    res.json(notes)
+
+    // TODO need to figure out how to return user name not just ID
+});
 
 // @desc Create new note
 // @route POST /notes
 // @access Private
-const createNewNote = asyncHandler(async (req, res) => {});
+const createNewNote = asyncHandler(async (req, res) => {
+    
+});
 
 // @desc Update existing note
 // @route PATCH /notes
